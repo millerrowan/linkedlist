d6a7a45e96f1cae407ec1c33b7fc2644cffde6cf
U-BSD\412170 <412170@SHS-95W2NW3.beaverton.k12.or.us>
2024-01-03 12:25:36 -0800

    commit 7

--- main.cpp
+++ main.cpp
@@ -12,8 +12,9 @@
 using namespace std;
 
 void add(Node* &head, Node* prev, Student* s, Node* nextNode);
-void print();
+void print(Node* head);
 void Delete();
+void average();
 
 int main() {
   
@@ -25,22 +26,26 @@ int main() {
   char input[10]; 
 
   while(stillPlaying == true) {
-    cout << "Enter 'ADD', 'PRINT', 'DELETE', or 'QUIT'" << endl;
+    cout << "Enter 'ADD', 'PRINT', 'DELETE', 'AVERAGE', or 'QUIT'" << endl;
     
     cin >> input;
     
     if(strcmp(input, "ADD")==0) {
       Student* s = new Student();
       Node* nextNode = new Node(s); 
-      add(head, prev, s, nextNode);
+      add(head, head, s, head);
     }
     
     if(strcmp(input, "PRINT")==0) {
-      print();
+      print(head);
     }
     
     if(strcmp(input, "DELETE")==0) {
       Delete();
+    }
+
+    if(strcmp(input, "AVERAGE")==0) {
+      average();
     }
     
     if(strcmp(input, "QUIT")==0) {
@@ -49,12 +54,19 @@ int main() {
   }
 }
 
-void add(Node* &head, Node* prev, Student* s, Node* nextNode) {
+void add(Node* &head, Node* prev, Student* s, Node* current) {
   //use recursion
-  Node* current = head;
-  if(current == NULL) {
-    current = new Node(s); 
+  if(head == NULL) {
+    head = new Node(s);
+    head->setNext(NULL); 
   }
+  //if student id is at the beginning of list
+  else if(s->getID() < head->getStudent()->getID()) {
+    Node* temp = new Node(s);
+    temp->setNext(head);
+    head = temp; 
+  }
+  //if student id is at the end of list
   else if(current->getNext() == NULL) {
     if(s->getID() < current->getStudent()->getID()) {
       Node* temp = new Node(s);
@@ -63,29 +75,52 @@ void add(Node* &head, Node* prev, Student* s, Node* nextNode) {
     }
     else {
       Node* temp = new Node(s); 
-      current->setNext(temp); 
+      current->setNext(temp);
+      temp->setNext(NULL); 
     }
   }
+  //if student id is in the middle of list
   else{
-    Node* prev = head;
-    Node* current = head->getNext(); 
-    while(current->getNext() != NULL) {
-      current = current->getNext(); 
+    if(s->getID() < current->getStudent()->getID()) {
+      Node* temp = new Node(s);
+      prev->setNext(temp);
+      temp->setNext(current);
     }
-    current->setNext(nextNode);//will have to change this variable name
-    current->getNext()->getStudent();//pass in variables from node.cpp 
+    else{  
+      add(head, current, s, current->getNext()); 
+    }
   }
 }
 
 				   
-void print() {
+void print(Node* head) {
   //use recursion
-  
-
+  Node* current = head;
+  if(current != NULL) {
+    cout << current->getStudent()->getFN() << " " << current->getStudent()->getLN() << ", " << current->getStudent()->getID();
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2); 
+    cout << ", " << current->getStudent()->getGPA() << endl;
+    print(current->getNext()); 
+  }
 }
 
-void Delete() {
+void Delete(Node* head) {
   //make sure to confirm the deletion
-
+  int stored; 
+  cout << "Type in the student id of the student you want to delete" << endl; 
+  cin >> stored;
+  Node* current = head;
+  if (current->getStudent()->getID() == stored) {
+    delete current;
+  }
+  else{
+    current =  
+    Delete(head);
+  }
 }
 
+void average() {
+
+
+}
